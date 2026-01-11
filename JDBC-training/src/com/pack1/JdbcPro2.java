f93d70ac6f7d0a586a03e01400a9539677a5acc8
Samarpit Maurya <samarpitmaurya4204@gmail.com>
2026-01-11 20:47:59 +0530

    update JdbcPro2 : use executeUpdate method and make connect method

--- JDBC-training/src/com/pack1/JdbcPro2.java
+++ JDBC-training/src/com/pack1/JdbcPro2.java
@@ -1,9 +1,7 @@
 package com.pack1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 public class JdbcPro2
 {
@@ -11,7 +9,39 @@ public class JdbcPro2
     String driverUrl = "jdbc:oracle:thin:@localhost:1521/FREEPDB1";
     String dbUser = "system";
     String dbPwd = "root";
+
+    Scanner sc = new Scanner(System.in);
+
     String sqlQuery = "select * from employee";
+    String sqlQuery2 = "insert into Employee values('107','Samarpit','Maurya', 45000, 'Rdr')";
+    String sqlQuery3 = "delete from employee where eid='108';";
+    String sqlQuery4 = "update employee set esal=65000 where eid='101';";
+
+
+
+//    ------------------------------ Connect Method ------------------------------
+
+    Connection connect()
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(driverUrl,dbUser,dbPwd);
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+//    ---------------------------------------------------------------------------------
+
+
+
+//    ------------------------- Get Employee Data method -------------------------------
 
     void getEmpData()
     {
@@ -38,9 +68,136 @@ public class JdbcPro2
         }
     }
 
+//----------------------------------------------------------------------------------------------
+
+
+
+
+//--------------------------------- Insert Employee Data method -----------------------------------
+
+    void insertEmpData()
+    {
+        System.out.println("Inserting the data into Employee table");
+
+        try
+        {
+
+            Connection con = connect();
+            Statement stmt = con.createStatement();
+            int rowCount = stmt.executeUpdate(sqlQuery2);
+            if (rowCount==0)
+            {
+                System.out.println("Data not inserted");
+            }
+            else
+            {
+                System.out.println(rowCount+" row is inserted");
+                System.out.println("--------------------------------");
+                getEmpData();
+            }
+
+        }
+
+        catch (SQLIntegrityConstraintViolationException sicve)
+        {
+            System.out.println("Duplicate EmpId's are not allowed");
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //-----------------------------------------------------------------------------------------------
+
+
+
+
+    //-------------------------------- Delete Employee Data method -----------------------------------
+
+    void deleteEmpData()
+    {
+        System.out.println("Deleting data from Employee Table");
+        try
+        {
+
+            Connection con = connect();
+            Statement stmt = con.createStatement();
+            int rowCount = stmt.executeUpdate(sqlQuery3);
+            if (rowCount==0)
+            {
+                System.out.println("There is NO Employee with given Eid");
+            }
+            else
+            {
+                System.out.println(rowCount+" row deleted");
+                System.out.println("Do you want to view the the table data ? (Y/N)");
+                char choice = sc.nextLine().charAt(0);
+                switch (choice)
+                {
+                    case 'y','Y':
+                        getEmpData();
+                        break;
+                    case 'n','N':
+                        System.exit(0);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+//-----------------------------------------------------------------------------------------------
+
+
+
+
+
+//-------------------------------- Update Employee Data method ---------------------------------
+
+    void updateEmpData()
+    {
+        System.out.println("Updating Emp data");
+        try
+        {
+                Connection con = connect();
+                Statement stmt = con.createStatement();
+                int rowCount = stmt.executeUpdate(sqlQuery4);
+                if (rowCount==0)
+                {
+                    System.out.println("given empId is NOT available");
+                }
+                else
+                {
+                    System.out.println("Salary Updated");
+                }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+//-----------------------------------------------------------------------------------------------
+
+
+
+
+
+//-------------------------------------- Main Method ------------------------------------------
+
+
     public static void main(String[] args)
     {
             JdbcPro2 obj = new JdbcPro2();
-            obj.getEmpData();
+//            obj.getEmpData();
+//            obj.insertEmpData();
+//            obj.deleteEmpData();
+//            obj.updateEmpData();
     }
 }
